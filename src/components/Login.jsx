fab2dfc092e8df295206cfbeca27fd576556f88b
lakshya1112 <lakshyasaxena272@gmail.com>
2025-07-21 00:50:37 +0530

    password regex change in loginh.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
   };
 
   const validatePassword = (password) => {
-    const regex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+    const regex = /^(?=.*[A-Za-z])(?=.*\d).{8,}$/;
     return regex.test(password);
   };
 
